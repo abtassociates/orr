bbd11edd8134c83be6cf150954a3c2e5b1116931
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2025-07-23 13:14:19 -0400

    some updates to coc_selection module. will defer to Marschall's version, but don't want to lose these changes

--- R/mod_coc_selection.R
+++ R/mod_coc_selection.R
@@ -7,30 +7,34 @@ mod_coc_selection_ui <- function(id) {
     card(
       card_header("Select your Continuum of Care"),
       selectInput(ns("coc_select"), "CoC Code",
-                  choices = c("Please select" = "", sort(unique(hic_data$CoC_Code)))),
+                  choices = c("Please select" = "", get_db_query("SELECT DISTINCT hudnum FROM all_hic_data ORDER BY hudnum")$hudnum)),
       actionButton(ns("next_btn"), "Next", class = "btn-primary")
     )
   )
 }
 
-mod_coc_selection_server <- function(id, nav_control, projects_data, selected_coc) {
+mod_coc_selection_server <- function(id, nav_control, selected_coc, projects_data) {
   moduleServer(id, function(input, output, session) {
     observeEvent(input$next_btn, {
       if (input$coc_select != "") {
-        selected_coc(input$coc_select)
         nav_control("inventory")
         
-        # Initialize projects data
-        filtered_data <- hic_data %>%
-          fsubset(CoC_Code == input$coc_select) %>%
-          fmutate(
-            DV_Renewal = NA_character_,
-            Grant_Number = NA_character_,
-            CoC_Funding_Requested = NA_real_,
-            Funding_Action = fifelse(McKinney_Vento == "No", "Ignore", "Renew")
-          )
+        data <- get_db_query(
+          "SELECT p.*, c.coc FROM projects p 
+             LEFT JOIN coc_instances c ON p.coc_instance_id = c.coc_instance_id 
+             WHERE c.coc = $1",
+          params = list(input$coc_select)
+        )
+        req(nrow(data) > 0)
         
-        projects_data(filtered_data)
+        # use factors to ensure dropdowns
+        user_columns <- c("dv_renewal", "grant_number", "coc_amount_awarded_last_year", "coc_amount_expended_last_year", "coc_funding_requested", "funding_action")
+        data <- data %>%
+          fselect(-coc_instance_id) %>%
+          ftransformv(c(user_columns, "project_type", "target_population", "mckinneyvento"), forcats::as_factor)
+        
+        selected_coc(input$coc_select)
+        projects_data(data)
       }
     })
   })
