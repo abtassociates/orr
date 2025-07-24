f5660fd6a9c0129e544744c357c1cc9fbef2f55c
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2025-07-24 12:29:12 -0400

    code cleanups: - duplicate cell_edit event - outline comments for better nav

--- R/mod_inventory.R
+++ R/mod_inventory.R
@@ -17,7 +17,7 @@ mod_inventory_server <- function(id, projects_data) {
   moduleServer(id, function(input, output, session) {
     user_columns <- c("dv_renewal", "grant_number", "coc_amount_awarded_last_year", "coc_amount_expended_last_year", "coc_funding_requested", "funding_action")
     
-    # Projects table
+    # Projects table -----
     output$projects_table <- renderDT({
       req(projects_data())
 
@@ -62,40 +62,20 @@ mod_inventory_server <- function(id, projects_data) {
       dt
     })
     
-    # Update projects data when cell is edited
+    # inline edit handling ------
     observeEvent(input$projects_table_cell_edit, {
       req(projects_data())
       info <- input$projects_table_cell_edit
-      str(info)
       
       # Get the current data
       data <- data.table::copy(projects_data())
-      
-      # Get the row index in the filtered view
       row_idx <- info$row
-      
-      # Get the actual row index in the full dataset
       actual_row <- which(data$project_name == projects_data()$project_name[row_idx])
-      
-      # Adjust column index since we removed CoC_Code
       col_name <- names(projects_data())[info$col + 1]
       data[actual_row, col_name] <- info$value
       
       projects_data(data)
     }, ignoreInit = TRUE)
     
-    # Update projects data when cell is edited
-    observeEvent(input$cell_edit, {
-      req(projects_data())
-      print("cell was edited")
-      data <- projects_data()
-      # Adjust column index since we removed CoC_Code
-      actual_col <- input$cell_edit$col + 1
-      if (actual_col >= which(names(data) == "CoC_Code")) {
-        actual_col <- actual_col + 1
-      }
-      data[input$cell_edit$row + 1, actual_col] <- input$cell_edit$value
-      projects_data(data)
-    }, ignoreInit = TRUE)
   })
 }
