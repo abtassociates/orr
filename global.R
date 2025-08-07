f789293ebdc2d589a959f2fd0c811ae51b183f8c
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2025-08-07 16:35:46 -0400

    define in_dev_mode earlier, as its used for R/utils

--- global.R
+++ global.R
@@ -18,12 +18,11 @@ library(shinyvalidate)
 library(purrr)
 library(shinycssloaders)
 
+in_dev_mode <- grepl("ad.abt.local", Sys.info()[["nodename"]]) & !isTRUE(getOption("shiny.testmode"))
 
 # Load all utils functions
 files <- list.files(here("R/utils"), pattern = "\\.R$", full.names = TRUE)
 lapply(files, source)
-
-in_dev_mode <- grepl("ad.abt.local", Sys.info()[["nodename"]]) & !isTRUE(getOption("shiny.testmode"))
 
 # Pull global datasets from db
 source(here("R/global_data_prep.R"))
