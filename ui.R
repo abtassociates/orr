d49618b1a0fd4fc416d1c39ab10005615da1fa00
ABT\furmanm <51181259+mlfurman3@users.noreply.github.com>
2025-09-17 08:22:58 -0400

    move About tab content into a card for easier positioning

--- ui.R
+++ ui.R
@@ -22,23 +22,29 @@ page_navbar(
   nav_panel(
     title = 'About',
     value = 'about',
-     ## log in button
-     tags$a(id = "login_link", "Log in", class = 'btn btn-primary', href = aws_auth_redirect),
-     ## create account button
-     tags$a(id = "signup_link", "Create Account", class = "btn btn-primary", href = aws_auth_signup),
-
-    h4('About the Tool'),
-    HTML("<p>HUD is providing this Rating and Ranking Tool to help CoCs design and implement a comprehensive annual CoC competition application review process. It has several customization features so you can choose the rating factors that are most relevant to your CoC and the priorities your CoC has adopted to inform system (re)design.</p>
-      <p>DISCLAIMER: HUD is explicitly stating that use of this tool is optional, is not being promoted over other tools CoCs currently use, and does not guarantee:</p>
-     <ul>
-     <li>additional points in the Fiscal Year (FY) 2024 Continuum of Care Program (CoC) Competition;</li>
-     <li>CoC applications will be consistent with all NOFO requirements; and</li>
-     <li>HUD will award CoCs with full points or funding.</li>
-     </ul>
-     <p>The tool provides a strong framework for implementing a data-driven rating process and a ranking process informed by system priorities and capacity analysis (if available) and it satisfies the objective criteria requirement in the FY 2024 CoC Program NOFO.  HUD strongly encourages CoCs to read the CoC Program NOFO carefully to determine if there are new opportunities, priorities, or expectations that your CoC might need to assess outside this tool.  The Priority Listing is the official project ranking record for the CoC Program NOFO.  HUD is not requiring CoCs to use this tool, nor is it preferred over other rating tools or processes, use of the tool does not guarantee additional points on the CoC Program application. HUD has made this tool available to CoCs for use in their year-round NOFO planning process.  Feedback on the tool is welcome.</p>"
-    )
-   
     
+    card(id = 'about_card',
+         card_header(h4('About the Tool')),
+         card_body(
+          fill = FALSE, fillable = FALSE,
+     
+          ## log in button
+          tags$a(id = "login_link", "Log in", class = 'btn btn-primary', href = aws_auth_redirect),
+          ## create account button
+          tags$a(id = "signup_link", "Create Account", class = "btn btn-primary", href = aws_auth_signup),
+          br(),
+        HTML("<p>HUD is providing this Rating and Ranking Tool to help CoCs design and implement a comprehensive annual CoC competition application review process. It has several customization features so you can choose the rating factors that are most relevant to your CoC and the priorities your CoC has adopted to inform system (re)design.</p>
+          <p>DISCLAIMER: HUD is explicitly stating that use of this tool is optional, is not being promoted over other tools CoCs currently use, and does not guarantee:</p>
+         <ul>
+         <li>additional points in the Fiscal Year (FY) 2024 Continuum of Care Program (CoC) Competition;</li>
+         <li>CoC applications will be consistent with all NOFO requirements; and</li>
+         <li>HUD will award CoCs with full points or funding.</li>
+         </ul>
+         <p>The tool provides a strong framework for implementing a data-driven rating process and a ranking process informed by system priorities and capacity analysis (if available) and it satisfies the objective criteria requirement in the FY 2024 CoC Program NOFO.  HUD strongly encourages CoCs to read the CoC Program NOFO carefully to determine if there are new opportunities, priorities, or expectations that your CoC might need to assess outside this tool.  The Priority Listing is the official project ranking record for the CoC Program NOFO.  HUD is not requiring CoCs to use this tool, nor is it preferred over other rating tools or processes, use of the tool does not guarantee additional points on the CoC Program application. HUD has made this tool available to CoCs for use in their year-round NOFO planning process.  Feedback on the tool is welcome.</p>"
+        )
+   
+      )
+    )
   ),
   
   #mod_requests_ui("requests"),
