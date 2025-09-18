59524dc5eb650007972959306c62f170220d9cef
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2025-09-18 19:03:21 -0400

    remove navbar from modalDialog theme. N/A

--- R/mod_inventory_add_project.R
+++ R/mod_inventory_add_project.R
@@ -39,11 +39,7 @@ mod_inventory_add_project_ui <- function(id, form_type = "New", project_to_repla
     title = title,
     size = "m",
     fluidPage(
-      theme =  bs_theme(
-        version = 5,
-        bootswatch = "zephyr",
-        navbar_bg = "#04153B"
-      ),
+      theme =  bs_theme(version = 5, bootswatch = "zephyr"),
       # -- Core Project Info --
       textInput(ns("project_name"), "Project Name*"),
       textInput(ns("organization_name"), "Organization Name*"),
