e0f1c0da8d8a17d7cd6b9d9385d09021be5a267b
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2025-09-30 15:01:44 -0400

    change Toggle button text

--- R/mod_inventory.R
+++ R/mod_inventory.R
@@ -159,7 +159,7 @@ mod_inventory_server <- function(id, nav_control, user_coc) {
         cols_to_disable = c("ch_bed_inventory", "vet_bed_inventory","youth_bed_inventory", "dv_fam_beds","dv_ind_beds"),
         buttons = list(
           list(
-            text="Toggle Bed Inventory",
+            text="Show/Hide Bed Inventory",
             action = DT::JS(sprintf("
               function ( e, dt, node, config ) {
                 var cols = %s;
