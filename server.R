9f180661126601173be759ffa346174ca27e96dc
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2025-11-04 20:50:33 -0500

    adding module_returns. For now, this is how we can talk across modules, since returning values is harder in a loop. This way, we can also set values anywhere and have them accessible everywhere.
    also made a number oc hanges to customize rating criteria and rating pages to get things to save and link together.
    
    for now, we're using module_returns to automatically update the factors seen when rating if the user goes back and modifies the customize rating criteria tab. Eventually, we probably want to actually pass this info around, rather than simply triggering a pull from the db. but this was the only way I could figure out to trigger a re-pull

--- server.R
+++ server.R
@@ -9,16 +9,19 @@ function(input, output, session) {
   )
   nav_control <- reactiveVal("about")
 
+  module_returns <- reactiveValues(customize_rating_criteria = 0)
+  
   observeEvent(user_coc$auth, {
     req(user_coc$auth)
     
     # Once user logs in, load the UI + Server functions of the desired modules
     lapply(TABS_AFTER_LOGIN, function(t) {
+      print(t)
       # UI
       nav_insert("nav", get(glue::glue("mod_{t}_ui"))(t), select = t == "dashboard")
       
       # Server
-      get(glue::glue("mod_{t}_server"))(t, nav_control, user_coc, parent_session = session)
+      get(glue::glue("mod_{t}_server"))(t, nav_control, user_coc, session, module_returns)
     })
   })
   
