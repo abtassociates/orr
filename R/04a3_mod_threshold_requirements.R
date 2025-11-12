c7222e4386372d1860eeb9929dc59679e0371ea0
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2025-11-11 20:54:29 -0500

    saving thresholds entries

--- R/04a3_mod_threshold_requirements.R
+++ R/04a3_mod_threshold_requirements.R
@@ -18,7 +18,7 @@ mod_threshold_requirements_ui <- function(id) {
       ),
       card_footer(
         style = "display: flex; justify-content: space-between; align-items: center;",
-        actionButton(ns("save_requirements"), "Save Thresholds", icon = icon("save"))
+        actionButton(ns("save_requirements"), "Save Thresholds", icon = icon("save"), class="btn-primary")
       )
     )
   )
@@ -56,19 +56,20 @@ mod_threshold_requirements_server <- function(id, user_coc, selected_project, se
         )
     })
     
-    # HUD Requirements UI
     load_selected_thresholds <- function(threshold_type) {
       thresholds <- threshold_entries() |>
         fsubset(type == threshold_type)
 
       lapply(1:nrow(thresholds), function(i) {
         checkboxInput(
-          paste0(threshold_type, "_req_", thresholds$threshold_id[i]),
+          ns(paste0(threshold_type, "_req_", thresholds$threshold_id[i])),
           label = thresholds$threshold_text[i],
           width = '100%'
         )
       })
     }
+    
+    # HUD Requirements UI
     output$hud_requirements <- renderUI({
       req(selected_project)
       load_selected_thresholds("HUD")
@@ -80,6 +81,27 @@ mod_threshold_requirements_server <- function(id, user_coc, selected_project, se
       load_selected_thresholds("CoC")
     })
     
+    observeEvent(input$save_requirements, {
+      req(user_coc$coc_version_id, user_coc$username)
+
+      update_data <- threshold_entries() |>
+        add_user_stamp(user_coc) |>
+        fmutate(
+          met_threshold = map2_lgl(type, threshold_id, ~input[[paste0(.x, "_req_", .y)]])
+        )
+      
+      for (i in 1:nrow(update_data)) {
+        row_data <- update_data[i, ]
+        update_query <- glue_data_sql(
+          "UPDATE threshold_entries 
+          SET met_threshold = {met_threshold}, created_by = {created_by}, updated_by = {created_by}
+          WHERE project_id = {selected_project};",
+          .con = DB_CON,
+          .envir = as.list(row_data)
+        ) 
+        DBI::dbExecute(DB_CON, update_query)
+      }
+    }, ignoreInit = TRUE)
     # Automatically set all HUD requirements to Yes when yes_to_all is checked
     # observeEvent(input$yes_to_all_hud, {
     #   if(input$yes_to_all_hud) {
