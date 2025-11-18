0822f586e0f2f75c3fb4668bd7db006c8c050899
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2025-11-18 14:20:42 -0500

    make the Request Access to a CoC pop-up a little wider and adjust label text

--- R/01a_mod_coc_selection.R
+++ R/01a_mod_coc_selection.R
@@ -366,10 +366,10 @@ mod_coc_selection_server <- function(id, nav_control, user_coc, parent_session) 
 
       showModal(modalDialog(
         title = 'Request Access to a CoC',
-        helpText('Select a CoC to view its versions...'),
-        selectInput(ns('request_access_coc_dropdown'),
-                    label = "Please choose a CoC:",
-                    choices = sort(funique(request_access_direct_coc_versions()$coc))
+        selectizeInput(
+          ns('request_access_coc_dropdown'),
+          label = "Please choose a CoC to view its versions:",
+          choices = sort(funique(request_access_direct_coc_versions()$coc))
         ),
         DT::DTOutput(ns("direct_request_coc_versions")),
         footer = tagList(
@@ -377,7 +377,8 @@ mod_coc_selection_server <- function(id, nav_control, user_coc, parent_session) 
           actionButton(ns('send_direct_request'), label='Send Request', disabled = TRUE),
           # If they cancel: close pop-up
           modalButton(label='Cancel')
-        )
+        ),
+        size = "l"
       ))
     })
 
