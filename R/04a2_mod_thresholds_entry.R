67b47f0c09625567017b4d3e3c6edf93a193d165
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2026-02-20 17:26:14 -0500

    refactoring threshold entry: - adding project_evaluation - updating checkboxes based on what's actually been selected in the customize section - hud requirements can be rendered directly in ui, whereas coc need to be dynamic because it depends on coc_verison and there may have been custom thresholds added - adding yes-to-all checkboxes for easier selection - fix how thresholds_to_enter is initialized based on selected project, and pull in necessary fields

--- R/04a2_mod_thresholds_entry.R
+++ R/04a2_mod_thresholds_entry.R
@@ -45,49 +45,81 @@ mod_thresholds_entry_server <- function(id, user_coc, selected_project, selected
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     
-    thresholds_to_enter <- reactive({
+    # These are the individual threshold entries
+    # These will be aggregated up to the project-level project-evaluations table
+    
+    updating_from_db <- reactiveVal(NA)
+    thresholds_to_enter <- reactiveVal(NULL)
+    project_evaluations <- reactiveVal(NULL)
+    
+    coc_thresholds_to_enter <- reactive({
+      thresholds_to_enter() |>
+        fsubset(type == "CoC" & selected)
+    })
+    
+    observe({
       req(user_coc$coc_version_id)
+      req(is.na(updating_from_db() || updating_from_db()))
+      req(selected_project())
 
-      get_db_query(
-        "SELECT st.selected_threshold_id, t.type, t.threshold_text, t.threshold_id, met_threshold, threshold_entry_id, te.date_updated
-        FROM thresholds t
-        LEFT JOIN selected_coc_thresholds st ON st.threshold_id = t.threshold_id
-        LEFT JOIN threshold_entries te ON te.threshold_id = t.threshold_id
-        WHERE st.coc_version_id = $1 OR t.type = 'HUD'
-        ",
-        params = list(user_coc$coc_version_id)
+      # individual threshold entries
+      thresholds_to_enter(
+        get_db_query(
+          "SELECT st.selected_threshold_id, st.selected, t.type, t.threshold_text, t.threshold_id, met_threshold, threshold_entry_id, te.date_updated
+          FROM thresholds t
+          LEFT JOIN selected_thresholds st ON st.threshold_id = t.threshold_id
+          LEFT JOIN threshold_entries te ON te.threshold_id = t.threshold_id
+          WHERE (st.coc_version_id = $1 OR t.type = 'HUD') AND (te.project_id = $2 OR te.project_id IS NULL)",
+          params = list(
+            user_coc$coc_version_id,
+            selected_project()$project_id
+          )
+        ) |>
+          fmutate(met_threshold = fcoalesce(as.logical(met_threshold), FALSE))
+      )
+      
+      # project-level evaluations
+      project_evaluations(
+        get_db_query(
+          "SELECT p.coc_version_id, pe.project_id, method, met_hud_thresholds, met_coc_thresholds, pe.date_updated 
+          FROM project_evaluations pe
+          LEFT JOIN projects p ON pe.project_id = p.project_id
+          WHERE coc_version_id = $1",
+          params = list(user_coc$coc_version_id)
+        )
+      )
+      
+      updateCheckboxGroupInput(
+        session,
+        "hud_requirements",
+        selected = thresholds_to_enter()[type == "HUD" & met_threshold]
+      )
+      
+      updateCheckboxGroupInput(
+        session,
+        "coc_requirements",
+        choices = setNames(
+          coc_thresholds_to_enter()$threshold_id, 
+          coc_thresholds_to_enter()$threshold_text
+        ),
+        selected = coc_thresholds_to_enter()[met_threshold == TRUE]
       )
     })
-    
-    load_selected_threshold_inputs <- function(threshold_type) {
-      thresholds <- thresholds_to_enter() |>
-        fsubset(type == threshold_type)
 
-      if(fnrow(thresholds) == 0){
-        return(NULL)  
-      } else {
-        lapply(1:nrow(thresholds), function(i) {
-          checkboxInput(
-            ns(paste0(threshold_type, "_req_", thresholds$threshold_id[i])),
-            value = fcoalesce(as.logical(thresholds$met_threshold[i]), FALSE),
-            label = thresholds$threshold_text[i],
-            width = '100%'
-          )
-        })
-      }
-      
-    }
-    
-    # HUD Requirements UI
-    output$hud_requirements <- renderUI({
-      req(selected_project)
-      load_selected_threshold_inputs("HUD")
-    })
-    
     # CoC Requirements UI
-    output$coc_requirements <- renderUI({
-      req(selected_project)
-      load_selected_threshold_inputs("CoC")
+    output$coc_requirements_ui <- renderUI({
+      req(selected_project())
+      
+      checkboxGroupInput(
+        ns("coc_requirements"),
+        label = NULL,
+        choices = setNames(
+          coc_thresholds_to_enter()$threshold_id, 
+          coc_thresholds_to_enter()$threshold_text
+        ),
+        selected = coc_thresholds_to_enter()[met_threshold == TRUE]$threshold_id,
+        width = "100%"
+      )
     })
     
     observeEvent(input$save_requirements, {
@@ -151,30 +183,59 @@ mod_thresholds_entry_server <- function(id, user_coc, selected_project, selected
     
     # Toggle HUD/CoC requirements when yes-to-all box is checked/unchecked
     yes_to_all <- reactiveValues()
-    lapply(c("hud","coc"), function(type) {
-      new_val <- input[[paste0("yes_to_all_", type)]]
-      observeEvent(new_val, {
+    lapply(c("hud","coc"), function(ttype) {
+      observeEvent(input[[paste0("yes_to_all_", ttype)]], {
+        new_val <- input[[paste0("yes_to_all_", ttype)]]
         req(new_val)
         
-        stored_val <- isolate(yes_to_all[[type]])
-        is_initialized <- !is.null(stored_val)
+        stored_val <- isolate(yes_to_all[[ttype]])
+        # is_initialized <- !is.null(stored_val)
         
         # Only update children if the user clicked (value changed from what we last recorded)
         if(!identical(new_val, stored_val)) {
-          yes_to_all[[type]] <- new_val
-          browser()
-          project_evaluations(
-            project_evaluations() |>
-              fmutate(paste0("met_", type, "_thresholds") := new_Val)
-          )
+          yes_to_all[[ttype]] <- new_val
           
           updateCheckboxGroupInput(
             session,
-            paste0(type, "_requirements"),
-            selected = if(new_Val) thresholds_to_enter()[type == toupper(type)] else NULL
+            paste0(ttype, "_requirements"),
+            selected = if(new_val) thresholds_to_enter()[type == ifelse(ttype == "coc", "CoC", "HUD") & selected]$threshold_id else NULL
           )
         }
       }, ignoreInit = TRUE)
     }) # end yes_to_all handler
+    
+    observe({
+      req(project_evaluations())
+      req(updating_from_db() == FALSE)
+      browser()
+      params_list <- project_evaluations() |>
+        fmutate(created_by = user_coc$username, new_date_updated = get_db_timestamp()) |>
+        fselect(project_id, method, met_hud_thresholds, met_coc_thresholds, created_by, new_date_updated, date_updated)
+        as.list() |> 
+        unname()
+      
+      tryCatch({
+        db_execute(
+          "INSERT INTO project_evaluations (project_id, method, met_hud_thresholds, met_coc_thresholds, created_by)
+            VALUES ($1, 'in-app', $2, $3, $4)
+            ON CONFLICT (project_id) DO UPDATE SET 
+              method = EXCLUDED.method,
+              met_hud_thresholds = EXCLUDED.met_hud_thresholds, 
+              met_coc_thresholds = EXCLUDED.met_coc_thresholds
+              date_updated = $5, 
+              updated_by = EXCLUDED.created_by
+            WHERE date_updated = $6;",
+          params = params_list
+        )
+        
+        showNotification("Project evaluations saved successfully!", type = "message", duration = 3)
+      }, error = function(e) {
+        # If an error occurs, do NOT reset the flag, so it will try again.
+        # Notify the user of the failure.
+        showNotification(paste("Error saving data:", e$message), type = "error", duration = 10)
+        cat("Database save error:", e$message, "\n")
+      })
+      
+    })
   }) # end moduleServer
 }
