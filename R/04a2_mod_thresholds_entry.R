8d9e91c9fc8e61c5f42a32f9e60faf3f949ab2f6
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2026-02-24 10:49:44 -0500

    THRESHOLD ENTRY CHANGES: - pulling out the update db code into functions - improving logging messages utility - using HUD and CoC throughout to facilitate subsetting and other things. - make sure to toggle yes_to_all based on underlying checkboxGroupInput - initializing CoC Requirements UI instead of slower renderUI. - save to db only when they hit Save. Then immediately re-pull data from db to update reactiveVals. Supposedly this isn't very costly, so why not to ensure consistency

--- R/04a2_mod_thresholds_entry.R
+++ R/04a2_mod_thresholds_entry.R
@@ -10,11 +10,11 @@ mod_thresholds_entry_ui <- function(id) {
         accordion_panel(
           "HUD Requirements",
           checkboxInput(
-            ns("yes_to_all_hud"),
+            ns("yes_to_all_HUD"),
             "Met all HUD requirements"
           ),
           checkboxGroupInput(
-            ns("hud_requirements"),
+            ns("HUD_requirements"),
             label = NULL,
             choices = setNames(
               HUD_THRESHOLD_REQUIREMENTS$threshold_id, 
@@ -26,15 +26,21 @@ mod_thresholds_entry_ui <- function(id) {
         accordion_panel(
           "CoC Requirements",
           checkboxInput(
-            ns("yes_to_all_coc"),
+            ns("yes_to_all_CoC"),
             "Met all CoC requirements"
           ),
-          uiOutput(ns("coc_requirements_ui"))
+          checkboxGroupInput(
+            ns("CoC_requirements"),
+            label = NULL,
+            width = "100%"
+          )
+          # uiOutput(ns("CoC_requirements_ui"))
         ),
         open = TRUE
       ),
       card_footer(
         style = "display: flex; justify-content: space-between; align-items: center;",
+        div(),
         actionButton(ns("save_requirements"), "Save Thresholds", icon = icon("save"), class="btn-primary")
       )
     )
@@ -48,19 +54,20 @@ mod_thresholds_entry_server <- function(id, user_coc, selected_project, selected
     # These are the individual threshold entries
     # These will be aggregated up to the project-level project-evaluations table
     
-    updating_from_db <- reactiveVal(NA)
+    # updating_from_db <- reactiveVal(NA)
     thresholds_to_enter <- reactiveVal(NULL)
     project_evaluations <- reactiveVal(NULL)
+    refresh_trigger <- reactiveVal(0)
     
     coc_thresholds_to_enter <- reactive({
       thresholds_to_enter() |>
         fsubset(type == "CoC" & selected)
     })
     
-    observe({
+    observeEvent(c(selected_project(), refresh_trigger()), {
       req(user_coc$coc_version_id)
-      req(is.na(updating_from_db() || updating_from_db()))
       req(selected_project())
+      # req(is.na(updating_from_db() || updating_from_db()))
 
       # individual threshold entries
       thresholds_to_enter(
@@ -84,41 +91,31 @@ mod_thresholds_entry_server <- function(id, user_coc, selected_project, selected
           "SELECT p.coc_version_id, pe.project_id, method, met_hud_thresholds, met_coc_thresholds, pe.date_updated 
           FROM project_evaluations pe
           LEFT JOIN projects p ON pe.project_id = p.project_id
-          WHERE coc_version_id = $1",
-          params = list(user_coc$coc_version_id)
+          WHERE p.coc_version_id = $1 and pe.project_id = $2",
+          params = list(user_coc$coc_version_id, selected_project()$project_id)
         )
       )
-      
-      updateCheckboxGroupInput(
-        session,
-        "hud_requirements",
-        selected = thresholds_to_enter()[type == "HUD" & met_threshold]
-      )
-      
-      updateCheckboxGroupInput(
-        session,
-        "coc_requirements",
-        choices = setNames(
-          coc_thresholds_to_enter()$threshold_id, 
-          coc_thresholds_to_enter()$threshold_text
-        ),
-        selected = coc_thresholds_to_enter()[met_threshold == TRUE]
-      )
-    })
 
-    # CoC Requirements UI
-    output$coc_requirements_ui <- renderUI({
-      req(selected_project())
+      updateCheckboxGroupInput(
+        session,
+        "HUD_requirements",
+        selected = thresholds_to_enter()[type == "HUD" & met_threshold]$threshold_id
+      )
       
-      checkboxGroupInput(
-        ns("coc_requirements"),
-        label = NULL,
+      updateCheckboxGroupInput(
+        session,
+        "CoC_requirements",
         choices = setNames(
-          coc_thresholds_to_enter()$threshold_id, 
+          coc_thresholds_to_enter()$threshold_id,
           coc_thresholds_to_enter()$threshold_text
         ),
-        selected = coc_thresholds_to_enter()[met_threshold == TRUE]$threshold_id,
-        width = "100%"
+        selected = coc_thresholds_to_enter()[met_threshold == TRUE]$threshold_id
+      )
+      
+      updateCheckboxInput(
+        session,
+        "yes_to_all_CoC",
+        value = allv(thresholds_to_enter()[type == "CoC"]$met_threshold, TRUE)
       )
     })
     
@@ -128,62 +125,52 @@ mod_thresholds_entry_server <- function(id, user_coc, selected_project, selected
       update_data <- thresholds_to_enter() |>
         add_user_stamp(user_coc, is_new = TRUE) |>
         fmutate(
-          met_threshold_new = fifelse(type == "CoC", threshold_id %in% input$coc_requirements, threshold_id %in% input$hud_requirements),
+          met_threshold_new = fifelse(type == "CoC", threshold_id %in% input$CoC_requirements, threshold_id %in% input$HUD_requirements),
           project_id = selected_project()$project_id, new_date_updated = get_db_timestamp()
         )
       
       if (nrow(update_data) > 0) {
-        params_list <- update_data |>
-          fsubset(met_threshold_new != met_threshold) |>
-          fselect(project_id, threshold_id, created_by, new_date_updated, date_updated) |> 
-          as.list() |> 
-          unname()
+        new_project_evaluations <- thresholds_to_enter() |> 
+          fmutate(project_id = selected_project()$project_id) |>
+          pivot(
+            ids = "project_id",
+            values = "met_threshold", 
+            names = "type",
+            how = "wider", 
+            FUN = all
+          ) |>
+          frename(\(x) ifelse(x == "project_id", x, paste0("met_", x, "_thresholds"))) |>
+          join(
+            project_evaluations() |> fselect(project_id, date_updated), 
+            on = "project_id"
+          )
         
-        tryCatch({
-          
-          db_execute(
-            "INSERT INTO threshold_entries (project_id, threshold_id, met_threshold, created_by)
-            VALUES ($1, $2, 1, $3)
-            ON CONFLICT (project_id, threshold_id) DO UPDATE SET 
-              met_threshold = EXCLUDED.met_threshold, 
-              date_updated = $4, 
-              updated_by = EXCLUDED.created_by
-            WHERE date_updated = $5;",
-            params = params_list
-          )
-          
-          ## update reactive ----------
-          thresholds_to_enter(
-            update_data |> 
-              fmutate(met_threshold = met_threshold_new) |>
-              fselect(-met_threshold_new)
-          )
-          
-          project_evaluations(
-            thresholds_to_enter() |> 
-              pivot(
-                ids = "project_id",
-                values = "met_threshold", 
-                names = "type",
-                how = "wider", 
-                FUN = all
-              ) |>
-              frename(\(x) ifelse(x == "project_id", x, paste0("met_", x, "_thresholds"))),
-          )
-          
-          showNotification("Threshold entires saved successfully!", type = "message", duration = 3)
-        }, error = function(e) {
-          # If an error occurs, do NOT reset the flag, so it will try again.
-          # Notify the user of the failure.
-          showNotification(paste("Error saving data:", e$message), type = "error", duration = 10)
-          cat("Database save error:", e$message, "\n")
+        pool::poolWithTransaction(DB_POOL, function(p) {
+          update_threshold_entries_db(p, update_data)
+          update_project_evaluations_db(p, new_project_evaluations, user_coc$username)
         })
+        
+        refresh_trigger(\(x) x + 1)
       }
     }, ignoreInit = TRUE)
     
+    toggle_yes_to_all <- function(type) {
+      num_thresholds_to_enter <- fnrow(thresholds_to_enter()[type == type])
+      num_thresholds_selected <- length(input[[paste0(type, "_requirements")]])
+      
+      updateCheckboxInput(
+        session,
+        paste0("yes_to_all_", type),
+        value = num_thresholds_to_enter == num_thresholds_selected
+      )
+    }
+    
+    observeEvent(input$HUD_requirements, toggle_yes_to_all("HUD"))
+    observeEvent(input$CoC_requirements, toggle_yes_to_all("CoC"))
+    
     # Toggle HUD/CoC requirements when yes-to-all box is checked/unchecked
     yes_to_all <- reactiveValues()
-    lapply(c("hud","coc"), function(ttype) {
+    lapply(c("HUD","CoC"), function(ttype) {
       observeEvent(input[[paste0("yes_to_all_", ttype)]], {
         new_val <- input[[paste0("yes_to_all_", ttype)]]
         req(new_val)
@@ -198,44 +185,70 @@ mod_thresholds_entry_server <- function(id, user_coc, selected_project, selected
           updateCheckboxGroupInput(
             session,
             paste0(ttype, "_requirements"),
-            selected = if(new_val) thresholds_to_enter()[type == ifelse(ttype == "coc", "CoC", "HUD")]$threshold_id else NULL
+            selected = if(new_val) thresholds_to_enter()[type == ttype]$threshold_id else NULL
           )
         }
       }, ignoreInit = TRUE)
     }) # end yes_to_all handler
     
-    observe({
-      req(project_evaluations())
-      req(updating_from_db() == FALSE)
-      browser()
-      params_list <- project_evaluations() |>
-        fmutate(created_by = user_coc$username, new_date_updated = get_db_timestamp()) |>
-        fselect(project_id, method, met_hud_thresholds, met_coc_thresholds, created_by, new_date_updated, date_updated)
-        as.list() |> 
-        unname()
-      
+    save_to_db <- function(p, sql, params, tbl_name) {
       tryCatch({
-        db_execute(
-          "INSERT INTO project_evaluations (project_id, method, met_hud_thresholds, met_coc_thresholds, created_by)
-            VALUES ($1, 'in-app', $2, $3, $4)
-            ON CONFLICT (project_id) DO UPDATE SET 
-              method = EXCLUDED.method,
-              met_hud_thresholds = EXCLUDED.met_hud_thresholds, 
-              met_coc_thresholds = EXCLUDED.met_coc_thresholds
-              date_updated = $5, 
-              updated_by = EXCLUDED.created_by
-            WHERE date_updated = $6;",
-          params = params_list
+        DBI::dbExecute(
+          p,
+          sql,
+          params = params
         )
-        
-        showNotification("Project evaluations saved successfully!", type = "message", duration = 3)
+        showNotification(glue::glue("{tbl_name} saved successfully!"), type = "message", duration = 3)
       }, error = function(e) {
         # If an error occurs, do NOT reset the flag, so it will try again.
         # Notify the user of the failure.
-        showNotification(paste("Error saving data:", e$message), type = "error", duration = 10)
+        browser()
+        showNotification(glue::glue("Error saving {tbl_name}: {e$message}"), type = "error", duration = 10)
         cat("Database save error:", e$message, "\n")
       })
-      
-    })
+                       
+    }
+    update_threshold_entries_db <- function(p, update_data) {
+      save_to_db(
+        p, 
+        "INSERT INTO threshold_entries (project_id, threshold_id, met_threshold, created_by)
+          VALUES ($1, $2, $3, $4)
+          ON CONFLICT (project_id, threshold_id) DO UPDATE SET 
+            met_threshold = EXCLUDED.met_threshold, 
+            date_updated = $5, 
+            updated_by = EXCLUDED.created_by
+          WHERE date_updated = $6;",
+        update_data |>
+          fsubset(met_threshold_new != met_threshold) |>
+          fselect(project_id, threshold_id, met_threshold, created_by, new_date_updated, date_updated) |> 
+          as.list() |> 
+          unname(),
+        "threshold_entries"
+      )
+    }
+    
+    update_project_evaluations_db <- function(p, new_project_evaluations, username) {
+      save_to_db(
+        p,
+        "INSERT INTO project_evaluations (project_id, method, met_hud_thresholds, met_coc_thresholds, created_by)
+          VALUES ($1, 'in_app', $2, $3, $4)
+          ON CONFLICT (project_id) DO UPDATE SET 
+            method = EXCLUDED.method,
+            met_hud_thresholds = EXCLUDED.met_hud_thresholds, 
+            met_coc_thresholds = EXCLUDED.met_coc_thresholds,
+            date_updated = $5, 
+            updated_by = EXCLUDED.created_by
+          WHERE date_updated = $6 OR ($5 IS NULL AND project_evaluations.date_updated IS NULL);",
+        new_project_evaluations |>
+          fmutate(
+            created_by = username, 
+            new_date_updated = get_db_timestamp()
+          ) |>
+          fselect(project_id, met_HUD_thresholds, met_CoC_thresholds, created_by, new_date_updated, date_updated) |>
+          as.list() |> 
+          unname(),
+        "project_evaluations"
+      )
+    }
   }) # end moduleServer
 }
