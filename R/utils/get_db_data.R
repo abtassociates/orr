656f27f41c0c2558cf83d2aeeb462682378ec468
Marschall Furman <furmanm@aneprrstu-01.ad.abt.local>
2026-02-26 13:25:20 -0500

    remove bracket conflict in get_db_data

--- R/utils/get_db_data.R
+++ R/utils/get_db_data.R
@@ -70,11 +70,7 @@ db_execute <- function(sql, params) {
   tryCatch({
     pool::poolWithTransaction(DB_POOL, function(p) {
       dbExecute(p, sql, params = params)
-<<<<<<< hotfix/saving-priorities
     }) 
-=======
-    })
->>>>>>> dev
   }, error = function(e) {
     list(ok = FALSE, error = e$message)
   })
