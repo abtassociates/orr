58dd679dd636b00fa1f1d3df8fb54449e4e5b9b4
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2026-03-17 15:25:46 -0400

    fixing bug in pulling all coc factors. Need to sequentially order the coc_version and funding_Action in params and in the placeholders

--- R/db_funcs/db_04a1b_mod_customize_rating_factors.R
+++ R/db_funcs/db_04a1b_mod_customize_rating_factors.R
@@ -1,22 +1,22 @@
 get_all_coc_factors <- function(funding_action_id, coc_version_id) {
   get_db_query(
     "SELECT rf.rating_factor_id, rf.funding_action, srf.selected, rf.project_type, rf.target_population, rf.rating_factor_text, COALESCE(srf.goal, rf.goal) AS goal,
-                 COALESCE(srf.max_point_value, rf.max_point_value) AS max_point_value, fg.factor_group, fsg.factor_subgroup, srf.date_updated
-          FROM rating_factors rf
-          JOIN factor_groups fg ON rf.factor_group = fg.factor_group_id
-          LEFT JOIN factor_subgroups fsg ON rf.factor_subgroup = fsg.factor_subgroup_id
-          LEFT JOIN selected_rating_factors srf ON rf.rating_factor_id = srf.rating_factor_id AND srf.coc_version_id = $2
-          WHERE rf.funding_action = $1 AND 
-            (rf.coc_version_id = $2 OR rf.coc_version_id IS NULL)",
-    params = list(funding_action_id, coc_version_id)
+           COALESCE(srf.max_point_value, rf.max_point_value) AS max_point_value, fg.factor_group, fsg.factor_subgroup, srf.date_updated
+    FROM rating_factors rf
+    JOIN factor_groups fg ON rf.factor_group = fg.factor_group_id
+    LEFT JOIN factor_subgroups fsg ON rf.factor_subgroup = fsg.factor_subgroup_id
+    LEFT JOIN selected_rating_factors srf ON rf.rating_factor_id = srf.rating_factor_id AND srf.coc_version_id = $1
+    WHERE rf.funding_action = $2 AND 
+      (rf.coc_version_id = $1 OR rf.coc_version_id IS NULL)",
+    params = list(coc_version_id, funding_action_id)
   )
 }
 
 get_other_factor_group_id <- function(funding_action_id) {
   get_db_query(
     "SELECT factor_group_id 
-         FROM factor_groups
-         WHERE factor_group = 'Other and Local Criteria' AND funding_action = $1", 
+    FROM factor_groups
+    WHERE factor_group = 'Other and Local Criteria' AND funding_action = $1", 
     params = funding_action_id
   )$factor_group_id
 }
@@ -24,10 +24,10 @@ get_other_factor_group_id <- function(funding_action_id) {
 get_subgroups_by_funding_action <- function(funding_action_id) {
   get_db_query(
     "SELECT sg.factor_subgroup, fg.factor_group
-            FROM factor_subgroups sg
-            RIGHT JOIN factor_groups fg ON fg.factor_group_id = sg.factor_group
-            WHERE fg.funding_action = $1
-          ", 
+    FROM factor_subgroups sg
+    RIGHT JOIN factor_groups fg ON fg.factor_group_id = sg.factor_group
+    WHERE fg.funding_action = $1
+    ", 
     params = funding_action_id
   )
 }
