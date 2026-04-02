5e1d365a8768192ee359ab46be3a6de7076eaf0f
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2026-04-02 16:22:43 -0400

    prevent user requesting access to the same version twice (by refreshing the reactive that stores their requests sent after they send one)

--- R/01a_mod_coc_selection.R
+++ R/01a_mod_coc_selection.R
@@ -31,9 +31,12 @@ mod_coc_selection_server <- function(id, nav_control, user_coc, parent_session, 
     
     all_versions_and_users <- reactiveVal()
     
-    refresh_trigger <- reactiveVal(0)
+    refresh_trigger <- reactiveValues(
+      versions = 0,
+      request_sent = 0,
+    )
     
-    observeEvent(c(user_coc$auth, refresh_trigger()), {
+    observeEvent(c(user_coc$auth, refresh_trigger$versions), {
       req(user_coc$auth)
       all_versions_and_users(
         get_all_coc_versions_and_users()
@@ -81,6 +84,8 @@ mod_coc_selection_server <- function(id, nav_control, user_coc, parent_session, 
     # Selected CoC Version Actions --------------
     ####
     requests_by_user <- reactive({
+      req(refresh_trigger$request_sent)
+
       get_all_requests_by_user(user_coc$username) |>
         fsubset(created_by == user_coc$username, coc_version_id, request_status)
     })
@@ -429,7 +434,7 @@ mod_coc_selection_server <- function(id, nav_control, user_coc, parent_session, 
       
       shiny::showNotification('New CoC version created!', type='message')
       removeModal()
-      refresh_trigger(\(x) x + 1)
+      refresh_trigger$versions <- refresh_trigger$versions + 1
     })
     
     #####################
@@ -513,7 +518,9 @@ mod_coc_selection_server <- function(id, nav_control, user_coc, parent_session, 
     # Create Version Request in DB, pass success to requests module, notify user
     observeEvent(input$send_request, {
       selected_version <- versions_and_requests_for_selected_coc()[input$existing_versions_rows_selected]
-      append_version_request(selected_version, user_coc)
+      s <- append_version_request(selected_version, user_coc)
+      if(isTruthy(s))
+        refresh_trigger$request_sent <- refresh_trigger$request_sent + 1
       
       ## TODO: send email to admin of version that is requested
       
