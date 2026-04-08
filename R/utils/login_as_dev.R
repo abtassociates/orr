1d221e86cc331c400067a18ef9739a1425b3bc0b
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2026-04-08 12:31:34 -0400

    make login-bypass more dynamic so we can login as any user. Add user role to users table, in case this comes up later

--- R/utils/login_as_dev.R
+++ R/utils/login_as_dev.R
@@ -1,19 +1,18 @@
-login_as_dev <- function(user_coc) {
-  devs <- list(
-    "silvermana" = list(
-      "email" = "alex.silverman@abtglobal.com",
-      "given_name" = "Alex"
-    ), 
-    "furmanm" = list(
-      "email" = "marschall.furman@abtglobal.com",
-      "given_name" = "Marschall"
+login_as_user <- function(user_coc, user_email = NULL) {
+
+  u <- if(is.null(user_email))
+    list(
+      username = "alex.silverman@abtglobal.com",
+      given_name = "Alex"
     )
-  )
+  else
+    get_db_tbl("users") |>
+      fsubset(username == user_email)
   
-  u <- devs[[Sys.info()[["user"]]]]
+  # u <- devs[[Sys.info()[["user"]]]]
   hideElement("login_link")
   hideElement("signup_link")
   user_coc$auth <- TRUE
-  user_coc$username <- u$email
+  user_coc$username <- u$username
   user_coc$given_name <- u$given_name
 }
