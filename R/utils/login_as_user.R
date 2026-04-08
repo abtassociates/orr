addf8166bd2ddaba5adf92d77fa06a191bcd8419
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2026-04-08 12:37:45 -0400

    by default, login as Sys.info() user (i.e. likely the developer)

--- R/utils/login_as_user.R
+++ R/utils/login_as_user.R
@@ -1,13 +1,16 @@
 login_as_user <- function(user_coc, user_email = NULL) {
-
-  u <- if(is.null(user_email))
-    list(
-      username = "alex.silverman@abtglobal.com",
-      given_name = "Alex"
-    )
-  else
-    get_db_tbl("users") |>
-      fsubset(username == user_email)
+  
+  devs <- list(
+    "furmanm" = "marschall.furman@abtglobal.com",
+    "silvermana" = "alex.silverman@abtglobal.com"
+  )
+  
+  u <- get_db_tbl("users") |>
+    fsubset(username == fifelse(
+      is.null(user_email),
+      devs[[Sys.info()[["user"]]]],
+      user_email
+    ))
   
   # u <- devs[[Sys.info()[["user"]]]]
   hideElement("login_link")
