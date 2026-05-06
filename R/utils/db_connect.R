4493cff0c1477ccd8d248d0435cbf3375b7442c3
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2026-05-05 20:00:13 -0400

    print list of databases

--- R/utils/db_connect.R
+++ R/utils/db_connect.R
@@ -199,24 +199,19 @@ get_db_name <- function(dbname = NULL) {
   return(dbname)
 }
 
-list_rpostgres_dbs <- function() {
-  db_connect(FALSE, dbname = "postgres")
-  DBI::dbGetQuery(get_db_pool(), "
+list_rpostgres_dbs <- function(dbname = "postgres") {
+  db_connect(FALSE, dbname)
+  message("Here are all the databases in RDS:")
+  print(DBI::dbGetQuery(get_db_pool(), "
     SELECT datname
     FROM pg_database
     WHERE datistemplate = false;
-  ")
+  "))
   close_pool()
 }
 db_connect <- function(use_sqlite = Sys.getenv("RSTUDIO") == "1", dbname = NULL) {
   USE_SQLITE <<- use_sqlite
-  
   db_pool <- set_up_db_connection(dbname)
-  
-  if(!use_sqlite & !is.null(db_pool)) {
-    message("Here are all the databases in RDS:")
-    list_rpostgres_dbs()
-  }
 }
 
 run_app <- function(use_sqlite = Sys.getenv("RSTUDIO") == "1", dbname = NULL, user_email = NULL) {
