53186272c5e622f8c4993b782ababb35cff99852
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2026-05-08 21:19:19 -0400

    fix saving inventory cols to hide by comma-separating them
    save created_by and updated_by as SERVICE_ACCOUNT
    
    Fix saving user settings

--- R/utils/user_settings.R
+++ R/utils/user_settings.R
@@ -1,12 +1,19 @@
 ## retrieve individual user setting value from DB
 get_user_setting <- function(p, setting_nm, coc_version_id, username){
   if(p$valid){
-    get_db_query(
-      "SELECT setting_value 
+    sql <- "SELECT setting_value 
       FROM user_settings 
-      WHERE coc_version_id = $1 AND coc_user = $2 AND setting_name = $3",
-      params = list(coc_version_id, username, setting_nm)
-    ) |> unlist(use.names = FALSE)
+      WHERE coc_version_id = $1 AND coc_user = $2 AND setting_name = $3"
+    
+    params <- list(coc_version_id, username, setting_nm)
+    
+    if(is.na(username)) {
+      sql <- gsub("$3","$2", gsub("AND coc_user = $2 ", "", sql, fixed=TRUE), fixed=TRUE)
+      params <- list(coc_version_id, setting_nm)
+    }
+    
+    get_db_query(sql, params = params) |>
+      unlist(use.names = FALSE)
   } else {
     return(character(0))
   }
@@ -19,34 +26,37 @@ update_user_coc_setting <- function(user_coc, setting_name, setting_value) {
     coc_version_id = user_coc$coc_version_id,
     coc_user = user_coc$username,
     setting_name = setting_name,
-    setting_value = setting_value
+    setting_value = paste(setting_value, collapse = ",")
   ) |>
     fmutate(
-      coc_user = fifelse(setting_name == "rating_method", NA, coc_user)
+      coc_user = fifelse(setting_name == "rating_method", NA, coc_user),
+      created_by = fifelse(setting_name == "rating_method", SERVICE_ACCOUNT, coc_user),
+      updated_by = fifelse(setting_name == "rating_method", SERVICE_ACCOUNT, coc_user)
     )
   
-  update_user_settings(get_db_pool(), updated_user_settings)
+  save_user_settings_db(get_db_pool(), updated_user_settings)
 }
 
 
 ## on app exit, update individual settings
-update_user_settings <- function(p, updated_user_settings){
-  save_to_db(
-    p,
-    "INSERT INTO user_settings (coc_version_id, coc_user, setting_name, setting_value, created_by, updated_by)
-    VALUES ($1, $2, $3, $4, $2, $2)
+save_user_settings_db <- function(p, updated_user_settings){
+  paramified <- paramify(updated_user_settings)
+  if(purrr::every(paramified, is.null)) 
+    return(FALSE)
+  
+  sql <- "INSERT INTO user_settings (coc_version_id, coc_user, setting_name, setting_value, created_by, updated_by)
+    VALUES ($1, $2, $3, $4, $5, $6)
     ON CONFLICT (coc_version_id, coc_user, setting_name) DO UPDATE SET
       setting_value = EXCLUDED.setting_value,
       updated_by = EXCLUDED.created_by,
       date_updated = CURRENT_TIMESTAMP
-    RETURNING user_setting_id, setting_name, setting_value, version_id",
-    updated_user_settings,
-    "user_settings"
-  )
+    RETURNING user_setting_id, setting_name, setting_value, version_id"
+  
+  DBI::dbExecute(p, sql, params = paramified)
 }
 
 ## on app exit, update all settings
-update_all_user_settings <- function(user_coc, tab_name){
+update_all_user_settings <- function(user_coc){
   # Make sure user is signed in and coc-version is selected
   if(!isolate(user_coc$auth))
     return(NULL)
