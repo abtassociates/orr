4dcb8718750c7a68a960902c5e94a9406832ecbf
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2026-05-14 10:17:58 -0400

    adding logger:: prefix to log_error calls in case the library isn't imported at runtime (like a clean session)

--- R/utils/get_db_data.R
+++ R/utils/get_db_data.R
@@ -13,7 +13,7 @@ get_db_query <- function(sql, params = NULL) {
     
     return(dt)
   }, error = function(e) {
-    log_error(paste0(sql, e$message))
+    logger::log_error(paste0(sql, e$message))
     list(ok = FALSE, error = e$message)
   })
 }
@@ -33,7 +33,7 @@ get_db_tbl <- function(tbl_name) {
     
     return(tbl)
   }, error = function(e) {
-    log_error(paste0("Importing ", tbl_name, e$message))
+    logger::log_error(paste0("Importing ", tbl_name, e$message))
     list(ok = FALSE, error = e$message)
   })
 }
@@ -47,7 +47,7 @@ db_execute <- function(sql, params = NULL) {
       DBI::dbExecute(get_db_pool(), sql, params = params)
     })
   }, error = function(e) {
-    log_error(paste0(sql, e$message))
+    logger::log_error(paste0(sql, e$message))
     list(ok = FALSE, error = e$message)
   })
 }
@@ -58,7 +58,7 @@ db_append <- function(tbl, data) {
       DBI::dbAppendTable(get_db_pool(), tbl, data)
     })
   }, error = function(e) {
-    log_error(paste0(sql, e$message))
+    logger::log_error(paste0(sql, e$message))
     list(ok = FALSE, error = e$message)
   })
 }
