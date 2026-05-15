794a50c013909988c792a1c34301762f5ea90008
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2026-05-14 21:54:02 -0400

    remove coc_cum after creating is_coc_eligible.

--- R/06_mod_ranking.R
+++ R/06_mod_ranking.R
@@ -502,6 +502,7 @@ mod_ranking_server <- function(id, nav_control, user_coc, parent_session, help_i
       if (any(dt$is_coc_eligible, na.rm=TRUE)) {
         dt[is_coc_eligible == TRUE, coc_cum := cumsum(coc_funding_recommendation)]
         dt[is_coc_eligible == TRUE & (coc_cum - coc_funding_recommendation) < coc_ard_data()$coc_bonus, coc_selected := TRUE]
+        dt[is_coc_eligible == TRUE, coc_cum := NULL]
       }
       
       dt[is_over_target == FALSE, dv_selected := FALSE]
@@ -765,7 +766,7 @@ mod_ranking_server <- function(id, nav_control, user_coc, parent_session, help_i
         )
     }
     
-    structural_cols <- c("project_id", "tier", "bonus_highlight", "coc_cum", "sort_project_type", "is_over_target", "straddle_amount")
+    structural_cols <- c("project_id", "tier", "bonus_highlight", "sort_project_type", "is_over_target", "straddle_amount")
     
     table_styles <- function(dt, type = "main") {
       dt <- dt |>
