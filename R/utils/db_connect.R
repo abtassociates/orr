579c0aec8664519688a1161e8a88f17960180ce0
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2026-07-23 11:39:23 -0400

    running into problem when closing the app in RStudio then trying to restart the app. Says "can't run `runApp` while within `runApp`".

--- R/utils/db_connect.R
+++ R/utils/db_connect.R
@@ -235,6 +235,20 @@ run_app <- function(use_sqlite = Sys.getenv("RSTUDIO") == "1", dbname = NULL, us
   USE_SQLITE <<- use_sqlite
   DEV_USER_LOGIN <<- user_email
   DBNAME <<- dbname
+  # 3. Post-exit Cleanup: Ensure globals are cleared when run_app stops or crashes
+  on.exit({
+    suppressWarnings(rm(USE_SQLITE, DEV_USER_LOGIN, DBNAME, envir = .GlobalEnv))
+    stopApp()
+    message("Cleaned up global variables.")
+  }, add = TRUE)
+  
   library(shiny)
+  
+  # 4. Shiny Server Shutdown Hook: Stops background processes when Shiny exits
+  shiny::onStop(function() {
+    stopApp()
+  })
+  
+  # Run the application
   runApp()
 }
